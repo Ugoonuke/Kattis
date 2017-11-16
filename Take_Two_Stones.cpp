d6a254a4da71d68cd7f73eb3efd49d37e0d66f6d
Ugo Onuke <ugoonuke@gmail.com>
2017-11-16 14:48:47 +0000

    Create Take_Two_Stones.cpp

---
+++ Take_Two_Stones.cpp
@@ -0,0 +1,20 @@
+/*
+* Take2Stones.cpp
+* Created by: Ugo Onuke
+* 16 Nov 2017
+* 14:32:49
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	if(n%2==1) cout<<"Alice";
+	else cout<<"Bob";
+	cout<<"\nEclipse C++"<<endl;
+	return 0;
+}
