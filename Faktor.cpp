50db53bd83beddb6e2e9cd863ffa8fdf499e43ca
Ugo Onuke <ugoonuke@gmail.com>
2017-11-18 16:40:44 +0000

    Create Faktor.cpp

---
+++ Faktor.cpp
@@ -0,0 +1,19 @@
+/*
+* faktor.cpp
+* Created by: Ugo Onuke
+* 18 Nov 2017
+* 16:32:15
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin>>a>>b;
+	cout<<(a*b)-(a-1);
+	cout<<"\nEclipse C++"<<endl;
+	return 0;
+}
