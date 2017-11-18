8fccd0a50717740daaca54f5bd077ad712d20c88
Ugo Onuke <ugoonuke@gmail.com>
2017-11-18 01:27:15 +0000

    Create Solving 4 Carrots.cpp

---
+++ Solving 4 Carrots.cpp
@@ -0,0 +1,23 @@
+/*
+* Solving4Carrots.cpp
+* Created by: Ugo Onuke
+* 18 Nov 2017
+* 01:16:21
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int n, p;
+	cin>>n>>p;
+	int string[n];
+	for(int i = 0; i < n; i++) {
+		cin>>string[i];
+	}
+	cout<<p;
+	cout<<"\nEclipse C++"<<endl;
+	return 0;
+}
