cb7fc8de2c8cc03039e9b9386abe3b0de68176ce
Ugo Onuke <ugoonuke@gmail.com>
2017-11-18 00:17:42 +0000

    Create Stuck In A Time Loop.cpp

---
+++ Stuck In A Time Loop.cpp
@@ -0,0 +1,21 @@
+/*
+* timeLoop.cpp
+* Created by: Ugo Onuke
+* 17 Nov 2017
+* 22:49:26
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	for(int i = 1; i <= n; i++) {
+		cout<<i<<" Abracadabra"<<endl;
+	}
+	cout<<"\nEclipse C++"<<endl;
+	return 0;
+}
